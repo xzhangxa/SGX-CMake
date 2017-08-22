0dedca8262d642d5385a4cf1132e2c725473b482
Xin Zhang <zhangxin.xa@gmail.com>
2017-08-22 10:37:11 +0800

    Use -fstack-protector-strong

--- cmake/FindSGX.cmake
+++ cmake/FindSGX.cmake
@@ -71,7 +71,7 @@ if(SGX_FOUND)
     endif()
 
     set(ENCLAVE_INC_FLAGS "-I${SGX_INCLUDE_DIR} -I${SGX_TLIBC_INCLUDE_DIR} -I${SGX_LIBCXX_INCLUDE_DIR}")
-    set(ENCLAVE_C_FLAGS "${SGX_COMMON_CFLAGS} -nostdinc -fvisibility=hidden -fpie -fstack-protector ${ENCLAVE_INC_FLAGS}")
+    set(ENCLAVE_C_FLAGS "${SGX_COMMON_CFLAGS} -nostdinc -fvisibility=hidden -fpie -fstack-protector-strong ${ENCLAVE_INC_FLAGS}")
     set(ENCLAVE_CXX_FLAGS "${ENCLAVE_C_FLAGS} -nostdinc++")
 
     set(APP_INC_FLAGS "-I${SGX_PATH}/include")
